5b5ef627de63f2ce36bafbda6b9c7556f02f7375
haisondang <haison.dang.1207@gmail.com>
2024-03-04 14:47:39 +0100

    init -charte

---
+++ src/main/java/fr/iut/mvcshop/MvcshopApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.mvcshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/iut/mvcshop/MvcshopApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.mvcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
